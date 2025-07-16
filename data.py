5f802b891768cdf10b77d810f794d420e35371fc
ZionChik5000 <supercreative5000@gmail.com>
2025-07-16 21:37:53 +0300

    Working camera and tile system

--- data.py
+++ data.py
@@ -5,19 +5,20 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 TILE_SIZE = 32
 
-cameraX = 0
-cameraY = 0
+cameraX = 0.0
+cameraY = 0.0
 cameraZoom = 1.0
-cameraXOffset = 0
-cameraYOffset = 0
+cameraXOffset = 0.0
+cameraYOffset = 0.0
+cameraFollowSpeed = 0.05
 
 
 player = {
     "name": "Krusty Krusador",
     "hp": 10,
     "dmg": 2,
-    "x": 0,
-    "y": 0,
+    "x": 10.0,
+    "y": 20.0,
     "speed": 5
 }
 
--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import pygame
 import sys
 import random
+import math
 from data import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Krust Krusador")
-tile = pygame.image.load("assets/tile.png")
 
 clock = pygame.time.Clock()
 running = True
+
+def selectTile(tileType):
+    match tileType:
+        case 0:
+            return pygame.image.load("assets/empty.png")
+        case 1:
+            return pygame.image.load("assets/tile.png")
+        case -1:
+            return pygame.image.load("assets/empty.png")
 
 def draw_map():
     for h in range(len(level["map"])):
         for w in range(len(level["map"][h])):
             screen.blit(
-                pygame.transform.scale(tile, (int(TILE_SIZE * cameraZoom), int(TILE_SIZE * cameraZoom))),
+                pygame.transform.scale(selectTile(level["map"][h][w]), (int(TILE_SIZE * cameraZoom), int(TILE_SIZE * cameraZoom))),
                 ((w * TILE_SIZE - cameraX) * cameraZoom + cameraXOffset,
                  (h * TILE_SIZE - cameraY) * cameraZoom + cameraYOffset)
             )
 
+def moveCamera():
+    global cameraX, cameraY
+    cameraX = cameraX + ((player["x"] - cameraX) * cameraFollowSpeed)
+    cameraY = cameraY + ((player["y"] - cameraY) * cameraFollowSpeed)
 
 while running:
     screen.fill(WHITE)
@@ -29,8 +42,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    moveCamera()
     draw_map()
-    
+
     pygame.display.flip()
     clock.tick(60)
     
