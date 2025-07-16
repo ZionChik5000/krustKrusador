02543de31328049b38b153502307eae40ef5111c
ZionChik5000 <supercreative5000@gmail.com>
2025-07-16 20:11:46 +0300

    config added

---
+++ config.py
@@ -0,0 +1,14 @@
+player = {
+    "name": "Krusty Krusador",
+    "hp": 10,
+    "dmg": 2,
+    "x": 0,
+    "y": 0,
+    "speed": 5
+}
+
+enemy = {
+    "name": "Bob: The Krusher of Krust",
+    "hp": 20,
+    "dmg": 3
+}
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
 import sys
+import random
+from config import *
 
 pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Krust Krusador")
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill(WHITE)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    
